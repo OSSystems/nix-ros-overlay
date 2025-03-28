a8903cc22d921b3285eeb9098a5acf9319a956fc
Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
2025-03-28 16:59:33 -0300

    distros/jazzy: Fix the compile flags to correct build of nav2-cstmap-2d
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
    (cherry picked from commit 6cf2d08c56124a344d7eeb14907f57c12d7fad17)

--- distros/jazzy/overrides.nix
+++ distros/jazzy/overrides.nix
@@ -221,6 +221,12 @@ in {
     '';
   });
 
+  nav2-costmap-2d = rosSuper.nav2-costmap-2d.overrideAttrs ({
+    NIX_CFLAGS_COMPILE ? [], ...
+  }: {
+    NIX_CFLAGS_COMPILE = NIX_CFLAGS_COMPILE ++ [ "-Wno-error" ];
+  });
+
   rviz-ogre-vendor = lib.patchAmentVendorGit rosSuper.rviz-ogre-vendor {
     url = "https://github.com/OGRECave/ogre.git";
     rev = "v1.12.10";
